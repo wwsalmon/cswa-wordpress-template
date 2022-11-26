aa037d9d967ada535fcb543e22189265cbc08c1e
Samson Zhang <wwsamson@yahoo.com>
2022-11-25 16:55:57 -0800

    navbar mobile responsive

--- header.php
+++ header.php
@@ -13,7 +13,11 @@ wp_head();
 </head>
 <body>
 <nav>
-    <img style="width: 64px; height: 64px;" src="<?php echo get_template_directory_uri() . '/img/logo.png'; ?>">
-    <span style="font-weight: 700; font-size: 28px; margin-left: 24px">CSWA</span>
-    <a href="" style="display: block; font-size: 20px; color: white; background-color: var(--accent-color); padding: 8px 16px; font-weight: 700; letter-spacing: 1px; text-transform: uppercase; margin-left: auto; text-decoration: none">Get involved</a>
+    <a href="<?php echo get_site_url(); ?>">
+        <img class="nav-logo" src="<?php echo get_template_directory_uri() . '/img/logo.png'; ?>">
+    </a>
+    <a href="<?php echo get_site_url(); ?>" style="color: #222; text-decoration: none">
+        <span class="nav-title">CSWA</span>
+    </a>
+    <a href="" class="nav-button">Get involved</a>
 </nav>
