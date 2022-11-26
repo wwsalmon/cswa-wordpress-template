18511f8cc7e7c1026a570848bbd2fb71f36c82b5
Samson Zhang <wwsamson@yahoo.com>
2022-11-25 16:04:04 -0800

    add hero

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php get_header();
 ?>
+    <div class="hero">
+        <div class="left-col">
+            <p class="h-uppercase">We are the</p>
+            <h1 style="font-weight: 400; font-size: 42px; margin: 16px 0">Claremont Student Worker Alliance</h1>
+            <p class="p-content" style="opacity: 0.75">Students working in thoughtful solidarity with organized labor fights at the 5Cs and Inland Empire</p>
+        </div>
+        <img class="right-col" src="<?php echo get_template_directory_uri() . '/img/hero.jpg'; ?>">
+    </div>
     <div class="container">
         <?php
         if (have_posts()):
