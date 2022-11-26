46e8a6edda9d3235f4d92ef3c6f113c2000463a1
Samson Zhang <wwsamson@yahoo.com>
2022-11-25 16:31:21 -0800

    add home blog feed

--- index.php
+++ index.php
@@ -1,21 +1,27 @@
 <?php get_header();
 ?>
     <div class="hero">
-        <div class="left-col">
+        <div class="hero-left">
             <p class="h-uppercase">We are the</p>
             <h1 style="font-weight: 400; font-size: 42px; margin: 16px 0">Claremont Student Worker Alliance</h1>
             <p class="p-content" style="opacity: 0.75">Students working in thoughtful solidarity with organized labor fights at the 5Cs and Inland Empire</p>
         </div>
-        <img class="right-col" src="<?php echo get_template_directory_uri() . '/img/hero.jpg'; ?>">
+        <img class="hero-right" src="<?php echo get_template_directory_uri() . '/img/hero.jpg'; ?>">
     </div>
-    <div class="container">
-        <?php
-        if (have_posts()):
-            while (have_posts()) : the_post();
-                get_template_part("template_parts/article");
-            endwhile;
-        endif;
-        ?>
+    <div class="home-container">
+        <div class="home-left">
+            <p class="h-uppercase">Latest updates</p>
+            <?php
+            if (have_posts()):
+                while (have_posts()) : the_post();
+                    get_template_part("template_parts/article");
+                endwhile;
+            endif;
+            ?>
+        </div>
+        <div class="home-right">
+            <p class="h-uppercase">@_cswa on Instagram</p>
+        </div>
     </div>
 <?php
 get_footer();
