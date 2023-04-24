5a9772434a25d6f75e34a88d5d6fd40be8fec824
Samson Zhang <wwsamson@yahoo.com>
2023-04-23 23:02:31 -0700

    add search

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <?php get_header();
 ?>
     <div class="container">
-        <h1>Latest updates</h1>
+        <h1>Search results for "<?php echo get_search_query() ?>"</h1>
         <?php
         get_search_form();
         if (have_posts()):
@@ -9,6 +9,8 @@
                 get_template_part("template_parts/article");
             endwhile;
             the_posts_pagination();
+        else:
+            ?><p style="margin-top: 16px;">No results found for search query.</p><?php
         endif;
         ?>
     </div>
